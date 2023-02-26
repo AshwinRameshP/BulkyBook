3adf3b00c68740b5d12a25fe417a512b946a868b
Ashwin <ashwinrameshp@gmail.com>
2023-02-26 22:52:08 +0530

    Added Delete Functionality.

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -26,6 +26,10 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+            }
             if (ModelState.IsValid) { 
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
@@ -33,5 +37,63 @@ namespace BulkyBookWeb.Controllers
             }
             return View();
         }
+        
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0){
+                return NotFound();
+            }
+            var categoryFromDb= _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid) { 
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if(id==null || id == 0){
+                return NotFound();
+            }
+            var categoryFromDb= _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null) {
+                return NotFound();
+            }
+             _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        
     }
 }
